725879f54b4e2b2b9faaf9ba3327ed7a69b19d73
ArtCode101 <sanayut12.service@gmail.com>
2025-09-28 14:41:50 +0700

    Example 2 add project Execution Engine JIT

---
+++ Example2/Execution_Engine_JIT/src/com/art/execution/engine/Main.java
@@ -0,0 +1,25 @@
+package com.art.execution.engine;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int iterations = 100;
+        int matrixSize = 500;
+
+        MatrixMultiplication matrix = new MatrixMultiplication();
+        double[][] a = matrix.createMatrix(matrixSize);
+        double[][] b = matrix.createMatrix(matrixSize);
+
+        for (int i = 0; i < iterations; i++) {
+            long startTime = System.nanoTime();
+            matrix.multiplyMatrices(a, b);
+            long endTime = System.nanoTime();
+            long durationInNanos = endTime - startTime;
+            double durationInSeconds = durationInNanos / 1_000_000.0;
+            System.out.printf("loop %d ,Time taken: %.4f milli milliseconds%n",i, durationInSeconds);
+        }
+
+        System.out.println("finished.");
+    }
+
+}
